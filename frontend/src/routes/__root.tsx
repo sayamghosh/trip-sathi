710f7d5cf936d38e3f1f86a31f13b2397baf2ecb
Sayam Ghosh <sayamghosh.dev@gmail.com>
2026-03-08 09:39:37 +0530

    scroll back to top when page change

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import ScrollRestoration from '../components/ScrollRestoration'
 
 export const Route = createRootRoute({
     component: () => (
         <>
+            <ScrollRestoration />
             <Navbar />
             <Outlet />
             <Footer />

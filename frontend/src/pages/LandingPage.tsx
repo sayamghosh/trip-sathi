9751d53acb78b08742b7bdc74577c8fd98efdef8
Sayam Ghosh <sayamghosh.dev@gmail.com>
2026-03-10 10:56:15 +0530

    why choose us section updated

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,10 +1,10 @@
 import Hero from '../components/Hero';
-import WhyChooseUs from '../components/WhyChooseUs';
 import PopularPackages from '../components/PopularPackages';
 import PlanVacation from '../components/PlanVacation';
 import BestSellingDestinations from '../components/BestSellingDestinations';
 import LimitedTimeDeals from '../components/LimitedTimeDeals';
 import MostVisitedCities from '../components/MostVisitedCities';
+import WhyTravelersChooseUs from '../components/WhyTravelersChooseUs';
 
 const LandingPage = () => {
     return (
@@ -14,7 +14,7 @@ const LandingPage = () => {
             <BestSellingDestinations />
             <LimitedTimeDeals />
             <MostVisitedCities />
-            <WhyChooseUs />
+            <WhyTravelersChooseUs />
             <PlanVacation />
         </div>
     );

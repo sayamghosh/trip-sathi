7b53046d7c2e3785a8972a9d8a88d52e3755b3b9
Sayam Ghosh <sayamghosh.dev@gmail.com>
2026-05-16 10:00:56 +0530

    50 remove the frontend react project (#51)
    * chore: frontend react project deleted
    
    * chore: update ignoreDeprecations to version 6.0 in tsconfig files

--- frontend/src/routes/allpackages/$id.lazy.tsx
+++
@@ -1,6 +0,0 @@
-import { createLazyFileRoute } from '@tanstack/react-router';
-import TravelerDetailedTourPlan from '../../components/TourPlans/TravelerDetailedTourPlan';
-
-export const Route = createLazyFileRoute('/allpackages/$id')({
-  component: TravelerDetailedTourPlan,
-});

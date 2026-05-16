86653ce0d7ff35390a5e10082e425c4071bb01aa
Sayam Ghosh <sayamghosh.dev@gmail.com>
2026-05-16 09:56:02 +0530

    chore: frontend react project deleted

--- frontend/src/routes/guide/tour-plans/$id.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute, Outlet } from '@tanstack/react-router';
-
-export const Route = createLazyFileRoute('/guide/tour-plans/$id')({
-    component: () => (
-        <div className="w-full">
-            <Outlet />
-        </div>
-    ),
-});
